809125415e921ac511928c7169cafbf4459f6702
raulrp3 <raul.ramirez@escuelaestech.es>
2019-07-07 17:42:14 +0200

    Creado nuevo componente y estableciendolo como principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PrincipalComponent]
 })
 export class AppModule { }
